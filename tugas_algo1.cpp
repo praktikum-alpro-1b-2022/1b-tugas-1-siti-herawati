75814e4e6eba2b4a0ba913756f2d3b615395912c
SitiHerawati1807 <114640259+SitiHerawati1807@users.noreply.github.com>
2022-10-06 14:09:44 +0700

    tugas no 1

---
+++ tugas_algo1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+   cout << "NPM      : 2210631170153" << endl;
+   cout << "nama     : Siti Herawati" << endl;
+   cout << "umur     : 19" << endl;
+   cout << "kelas    : 1B Informatika" << endl;
+   cout << "hobi     : membaca novel" << endl;
+   cout << "cita-cita: wanita karier" << endl;
+
+
+    return 0;
+}
